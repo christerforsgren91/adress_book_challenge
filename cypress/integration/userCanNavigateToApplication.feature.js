f2bc7ce33e8b9aa7c93bbdf81625f58e43dbee4b
Christer <christerforsgren91@gmail.com>
2021-03-24 14:06:58 +0100

    added som code in html and my test file and runned a few tests

--- cypress/integration/userCanNavigateToApplication.feature.js
+++ cypress/integration/userCanNavigateToApplication.feature.js
@@ -1,6 +1,19 @@
-describe('Application', () => {
-    it('is expected to display message', () => {
+describe('user can create a contact', () => {
+    it('test', () => {
         cy.visit('/')
-        cy.get('h1').should('contain.text', 'Adress Book')
+        cy.get('#add-contact').click()
+        cy.get('#name').type('Thomas')
+		cy.get('#email').type('thomas@craft.se')
+		cy.get('#phone').type('0700 101010')
+		cy.get('#company').type('Craft Academy')
+		cy.get('#notes').type('Awesome coder')
+        cy.get('#twitter').type('@thomasochman')
+        cy.get('#submit').click()
+    })
+    it('display a name of the new contact', () => {
+        cy.get('#contact-list').should('contain', 'Thomas')
+    })
+    it('displays the phone number of the new contact', () => {
+        cy.get('contact-list').should('contain', '0700 101010')
     })
 })
